8136f116eaf17da802d347907794f5a114f9d666
Lucas Nunes Dumas <128542655+wonalucard354@users.noreply.github.com>
2024-05-21 13:21:14 -0300

    calculadoraprojeto
    sempre bom salvar cada avanço seu

---
+++ calculadoraprojeto/calculadora.js
@@ -0,0 +1,25 @@
+function inserir(num){
+    document.querySelector(".tela").innerHTML += num;
+}
+
+function clean(){
+    document.querySelector(".tela").innerHTML = "";
+}
+
+function back(){
+const tela = document.querySelector(".tela").innerHTML;
+document.querySelector(".tela").innerHTML =tela.substring(
+0,
+tela.length - 1  
+);
+}
+
+function somar(){
+    tela = document.querySelector(".tela").innerHTML;
+
+    if (tela){
+    document.querySelector(".tela").innerHTML = eval(tela);
+    } else {
+        document.querySelector(".tela").innerHTML = "erro";
+    }
+}
